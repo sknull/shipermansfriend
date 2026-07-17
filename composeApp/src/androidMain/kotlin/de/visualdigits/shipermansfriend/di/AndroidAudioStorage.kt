7a813053e3e6877a02f9a7518bda62809466cb75
Stephan Knull <s.knull@t-online.de>
2026-07-17 14:56:13 +0200

    Try to fix R8 shrinking issues with audio files. Part 42 - mention mp3 resources explicitly in Country enum to prevent being thrown away by R8 optimizer.

--- composeApp/src/androidMain/kotlin/de/visualdigits/shipermansfriend/di/AndroidAudioStorage.kt
+++ composeApp/src/androidMain/kotlin/de/visualdigits/shipermansfriend/di/AndroidAudioStorage.kt
@@ -3,40 +3,45 @@ package de.visualdigits.shipermansfriend.di
 import android.content.Context
 import co.touchlab.kermit.Logger
 import java.io.File
+import java.io.FileNotFoundException
 
 class AndroidAudioStorage(private val context: Context) : AudioStorage {
 
     override suspend fun prepareAudio(fileName: String?): String? {
-        return if (fileName != null) {
-            try {
-                val tempFile = File(context.cacheDir, "temp_$fileName")
-                Logger.i("Prepare audio '$fileName', tempFile: '$tempFile'")
+        if (fileName == null) return null
+        return try {
+            val cleanFileName = fileName.lowercase()
+            val tempFile = File(context.cacheDir, "temp_$cleanFileName")
 
-                if (!tempFile.exists()) {
-                    Logger.i("Cached audio '$fileName', tempFile: '$tempFile'")
+            if (!tempFile.exists()) {
+                val assetManager = context.assets
 
-                    val assetManager = context.assets
-                    val primaryPath = "composeResources/de.visualdigits.compose.resources/files/$fileName"
-                    val fallbackPath = "composeResources/files/$fileName"
-                    val inputStream = try {
-                        assetManager.open(primaryPath)
-                    } catch (_: java.io.FileNotFoundException) {
-                        Logger.i("Primary path not found, trying fallback path for '$fileName'")
-                        assetManager.open(fallbackPath)
-                    }
-                    inputStream.use { ins ->
-                        tempFile.outputStream().use { outs ->
-                            ins.copyTo(outs)
-                        }
+                // workaround a weird behaviour of the android build process to manipulkate filenames al gusto
+                val lowercasePath = "composeResources/de.visualdigits.compose.resources/files/${fileName.lowercase()}"
+                val uppercasePath = "composeResources/de.visualdigits.compose.resources/files/${fileName.uppercase()}"
+
+                val inputStream = try {
+                    Logger.i("Prepare audio file '$lowercasePath'")
+                    assetManager.open(lowercasePath)
+                } catch (_: FileNotFoundException) {
+                    Logger.i("Prepare audio file '$uppercasePath'")
+                    try {
+                        assetManager.open(uppercasePath)
+                    } catch (_: okio.FileNotFoundException) {
+                        Logger.e("Neither '$lowercasePath' or '$uppercasePath' exists")
+                        null
                     }
                 }
-
-                tempFile.absolutePath
-            } catch (e: Exception) {
-                Logger.e("FAILED to prepare audio '$fileName': ${e.message}", e)
-                null
+                inputStream?.use { ins ->
+                    tempFile.outputStream().use { outs ->
+                        ins.copyTo(outs)
+                    }
+                }
             }
-        } else {
+
+            tempFile.absolutePath
+        } catch (e: Exception) {
+            Logger.e("Failed to prepare audio '$fileName': ${e.message}", e)
             null
         }
     }
--- composeApp/src/jvmMain/kotlin/de/visualdigits/shipermansfriend/di/DesktopAudioStorage.kt
+++ composeApp/src/jvmMain/kotlin/de/visualdigits/shipermansfriend/di/DesktopAudioStorage.kt
@@ -6,22 +6,19 @@ import de.visualdigits.compose.resources.Res
 class DesktopAudioStorage(private val homeDirectory: String) : AudioStorage {
 
     override suspend fun prepareAudio(fileName: String?): String? {
-        return if (fileName != null) {
-            try {
-                val tempFile = java.io.File(homeDirectory, "temp_$fileName")
-                Logger.i("Prepare audio '$fileName', tempFile: '$tempFile'")
-                if (!tempFile.exists()) {
-                    Logger.i("Cached audio '$fileName', tempFile: '$tempFile'")
-                    val bytes = Res.readBytes("files/$fileName")
-                    tempFile.writeBytes(bytes)
-                }
-
-                tempFile.toURI().toString()
-            } catch (e: Exception) {
-                Logger.e("Could not cache audio '$fileName'", e)
-                null
+        if (fileName == null) return null
+        return try {
+            val tempFile = java.io.File(homeDirectory, "temp_$fileName")
+            Logger.i("Prepare audio '$fileName', tempFile: '$tempFile'")
+            if (!tempFile.exists()) {
+                Logger.i("Cached audio '$fileName', tempFile: '$tempFile'")
+                val bytes = Res.readBytes("files/$fileName")
+                tempFile.writeBytes(bytes)
             }
-        } else {
+
+            tempFile.toURI().toString()
+        } catch (e: Exception) {
+            Logger.e("Could not cache audio '$fileName'", e)
             null
         }
     }
